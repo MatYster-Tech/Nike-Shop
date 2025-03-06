deb1a37eea78704a05a58b9ddc0e55dfc6367061
MatYster-Tech <mateusz.suchomski4@gmail.com>
2025-03-06 19:29:03 +0100

    second commit

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -10,14 +10,15 @@ const Products = () => {
           <img
             src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
             alt="Shoe"
+            className="card-img"
           />
           <div className="card-details">
             <h3 className="card-title">
               <section className="card-reviews">
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
+                <AiFillStar className="ratings-stars" />
+                <AiFillStar className="ratings-stars" />
+                <AiFillStar className="ratings-stars" />
+                <AiFillStar className="ratings-stars" />
                 <span className="total-reviews">4</span>
               </section>
               <section className="card-price">
@@ -25,7 +26,7 @@ const Products = () => {
                   <del>$300</del> 200
                 </div>
                 <div className="bag">
-                  <BsFillBagHeartFill />
+                  <BsFillBagHeartFill className="bag-icon" />
                 </div>
               </section>
             </h3>
