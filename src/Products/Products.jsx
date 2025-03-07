18990247a59a58bafaddb72dd93be11d2ccdd652
MatYster-Tech <mateusz.suchomski4@gmail.com>
2025-03-07 23:13:34 +0100

    07/03/2

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,37 +1,16 @@
-import { AiFillStar } from "react-icons/ai";
 import "./Products.css";
-import { BsFillBagHeartFill } from "react-icons/bs";
+import Card from "../components/Card";
 
 const Products = () => {
   return (
     <>
       <section className="card-container">
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">
-              <section className="card-reviews">
-                <AiFillStar className="ratings-stars" />
-                <AiFillStar className="ratings-stars" />
-                <AiFillStar className="ratings-stars" />
-                <AiFillStar className="ratings-stars" />
-                <span className="total-reviews">4</span>
-              </section>
-              <section className="card-price">
-                <div className="price">
-                  <del>$300</del> 200
-                </div>
-                <div className="bag">
-                  <BsFillBagHeartFill className="bag-icon" />
-                </div>
-              </section>
-            </h3>
-          </div>
-        </section>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,35 @@
+import { BsFillBagHeartFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
+
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <section className="card">
+      <img
+        src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+        alt="Shoe"
+        className="card-img"
+      />
+      <div className="card-details">
+        <h3 className="card-title">
+          <section className="card-reviews">
+            <AiFillStar className="ratings-stars" />
+            <AiFillStar className="ratings-stars" />
+            <AiFillStar className="ratings-stars" />
+            <AiFillStar className="ratings-stars" />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del> 200
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className="bag-icon" />
+            </div>
+          </section>
+        </h3>
+      </div>
+    </section>
+  );
 };
 
 export default Card;
