c445e3fef201f1c2a2953ee3dded4cd7578c5da2
MatYster-Tech <mateusz.suchomski4@gmail.com>
2025-03-10 21:05:40 +0100

    10/03/1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,71 @@
+import { useState } from "react";
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
+//Database
+import products from "./db/data";
+import Card from "./components/Card";
+
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
+
+  // ---------- Input Filter ----------
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filteredItems = products.filter((product) =>
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+  );
+
+  // ---------- Radio Filter ----------
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  // ---------- Buttons Filter ----------
+
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  function filtredData(products, selected, query) {
+    let filteredProducts = products;
+
+    //Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Selected Filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={crypto.randomUUID()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
   return (
     <>
       <Sidebar />
--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -5,6 +5,9 @@ const Colors = () => {
     <div>
       <h2 className="sidebar-title color-title">Colors</h2>
       <Input />
+      <Input />
+      <Input />
+      <Input />
     </div>
   );
 };
